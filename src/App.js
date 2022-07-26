2ac45cfbd0baa99033187669dd90764b6f34d832
Gilson Gabriel <gilsongabr@gmail.com>
2022-07-25 20:13:42 -0400

    - Relembrando alguns conceitos de React.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HelloWorld />
+      <SayMyName nome="Gilson" />
+      <SayMyName nome="Gabriel" />
+      <Pessoa nome="Gilson Gabriel" idade="27" profissao="Desenvolvedor Web" foto="https://via.placeholder.com/150" />
     </div>
   );
 }
